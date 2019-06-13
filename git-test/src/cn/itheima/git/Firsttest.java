16969843b955973e9265332e6383dfe20a280cc4
peiqi <542562655@qq.com>
2019-06-13 15:47:52 +0800

    把本地的工程添加到git

---
+++ git-test/src/cn/itheima/git/Firsttest.java
@@ -0,0 +1,11 @@
+package cn.itheima.git;
+
+public class Firsttest {
+    public static void main(String[] args) {
+        System.out.println("hello,git,project");
+        System.out.println("hello,git,project");
+        System.out.println("hello,git,project");
+        System.out.println("hello,git,project");
+        System.out.println("hello,git,project");
+    }
+}
